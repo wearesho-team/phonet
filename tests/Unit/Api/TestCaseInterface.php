9d9a829a3a0335339ac42a1671fb1c31101fb0d7
Kartvaik <roman.varkuta@gmail.com>
2019-02-28 14:01:31 +0200

    Separate logic from api test case to trait

---
+++ tests/Unit/Api/TestCaseInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Wearesho\Phonet\Tests\Unit\Api;
+
+/**
+ * Interface TestCaseInterface
+ * @package Wearesho\Phonet\Tests\Unit\Api
+ */
+interface TestCaseInterface
+{
+    public const REQUEST = 'request';
+    public const DOMAIN = 'test.phonet.com.ua';
+    public const API_KEY = 'test-api-key';
+    public const SESSION_ID = 'test-session-id';
+    public const EXPIRED_SESSION_ID = 'test-expired-session-id';
+}
